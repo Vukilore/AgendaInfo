a8a49e4ba26b4fd941496e0dffd7fe109c5be7b2
Quentin SENTE <quentin.sente@condorcet.be>
2020-05-14 15:59:56 +0200

    Changement de quelques Data Annoation et modif tag helpers

--- AgendaInfo/Models/POCO/DayOff.cs
+++ AgendaInfo/Models/POCO/DayOff.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,12 @@ namespace Agenda.Models.POCO
 
         /***************************PROPRIETES*******************************/
         public int ID { get; set; }                 // ID du congé
+        [Required]
         public DateTime StartDate { get; set; }     // Date/Heure du début du congé
+        [Required]
         public DateTime EndDate { get; set; }       // Date/Heure de fin du congé
+        
+        [MaxLength(244,ErrorMessage="244 caractère maximum")]
         public string Reason { get; set; }          // Raison personel du congé
 
         /***************************Constructeur*******************************/
