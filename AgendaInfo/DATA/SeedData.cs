aa38486feccbbf20ce563ab93b788740a51bca52
Quentin SENTE <quentin.sente@condorcet.be>
2020-05-23 23:04:40 +0200

    ok

--- AgendaInfo/DATA/SeedData.cs
+++ AgendaInfo/DATA/SeedData.cs
@@ -35,51 +35,13 @@ namespace AgendaInfo.DATA
                         Birthday = new DateTime(1994, 21, 04),
                         PhoneNumber = 6546546
                     });
-
                     // Création des utilisateurs
-                    c1 = new Customer
+                    context.User.Add(new Customer
                     {
-                        Name = "Masset",
-                        FirstName = "Laurent",
-                        Email = "lmasser@condorcet.be",
-                        Password = "1234",
-                        Address = "Rue de la montagne",
-                        Birthday = new DateTime(1902, 03, 07),
-                    };
-                    context.User.Add(c1);
-                    c2 = new Customer
-                    {
-                        Name="Copin",
-                        FirstName="Brigitte",
-                        Email="bcopin@condorcet.be",
-                        Password="5678",
-                        Address="Chemin du milieu",
-                        Birthday = new DateTime(1902, 03, 07),
-                    };
-                    context.User.Add(c2);
-                    c3 = new Customer
-                    {
-                        Name = "Vandevorst",
-                        FirstName = "Anne",
-                        Email = "avdv@condorcet.be",
-                        Password = "5678",
-                        Address = "Rue de l'automne",
-                        Birthday = new DateTime(1902, 03, 07),
-                    };
-                    context.User.Add(c3);
-                    c4 = new Customer
-                    {
-                        Name = "Clini",
-                        FirstName = "Dorian",
-                        Email = "dclini@condorcet.be",
-                        Password = "1234",
-                        Address = "Rue du berger",
-                        Birthday = new DateTime(1902, 03, 07),
-                    };
-                    context.User.Add(c4);
+
+                    });
                 }
 
-                // Création des services
                 if (!context.Service.Any())
                 {
                     s1 = new Service
@@ -105,85 +67,19 @@ namespace AgendaInfo.DATA
                     context.Service.Add(s3);
                 }
 
-                // Création de rendez-vous
                 if(!context.RendezVous.Any())
                 {
-                    context.RendezVous.Add(new RendezVous
-                    {
-                        Customer = c1,
-                        Service = s2,
-                        Comment = "Gros chien à l'entrée",
-                        BeginDate = new DateTime(2020, 28, 05, 10, 0, 0),
-                    });
-                    context.RendezVous.Add(new RendezVous
-                    {
-                        Customer = c4,
-                        Service = s3,
-                        Comment = "",
-                        BeginDate = new DateTime(2020, 29, 05, 16, 0, 0),
-                    });
-                    context.RendezVous.Add(new RendezVous
-                    {
-                        Customer = c2,
-                        Service = s1,
-                        Comment = "La maison se trouve dans le fond de l'allée",
-                        BeginDate = new DateTime(2020, 05, 06, 13, 0, 0),
-                    }) ;
-                    context.RendezVous.Add(new RendezVous
-                    {
-                        Customer = c3,
-                        Service = s2,
-                        Comment = "Chien dangereux",
-                        BeginDate = new DateTime(2020, 11, 06, 9, 0, 0),
-                    });
+                        context.RendezVous.Add(new RendezVous
+                        {
+                            // atributs
+                        });
+
                 }
 
-                // Création des évaluations
-                if (!context.Evaluation.Any())
-                {
-                    context.Evaluation.Add(new Evaluation
-                    {
-                        Rate =4,
-                        Comment = "Super, rapide et efficace",
-                    });
-                    context.Evaluation.Add(new Evaluation
-                    {
-                        Rate = 3,
-                        Comment = "Est arrivé en retard mais a été efficace",
-                    });
-                    context.Evaluation.Add(new Evaluation
-                    {
-                        Rate = 5,
-                        Comment = "Rien à redire, très professionel",
-                    });
-                }
-                // Création des jours de congé 
-                if (!context.DayOff.Any())
-                {
-                    context.DayOff.Add(new DayOff
-                    {
-                        StartDate= new DateTime (2020, 11, 06, 12, 0, 0),
-                        Reason = "Personnel",
-                    });
-                    context.DayOff.Add(new DayOff
-                    {
-                        StartDate = new DateTime(2020, 11, 06, 13, 0, 0),
-                        Reason = "Personnel",
-                    });
-                    context.DayOff.Add(new DayOff
-                    {
-                        StartDate = new DateTime(2020, 28, 05, 8, 0, 0),
-                        Reason = "",
-                    });
-                    context.DayOff.Add(new DayOff
-                    {
-                        StartDate = new DateTime(2020, 28, 05, 9, 0, 0),
-                        Reason = "",
-                    });
-                }
+                /* EVALUTATIONS, DAY OFF */
                 
-                context.SaveChanges();
-                
+                    context.SaveChanges();
+                }
             }
         }
     }
